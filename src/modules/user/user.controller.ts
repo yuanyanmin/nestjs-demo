64121add499b55659982f29a7ed75bb46132636d
yuanyanmin <yuanyanmin@pachiratech.com>
2023-09-20 18:37:29 +0800

    守卫使用

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from 'src/interfaces/user.interface';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { Role } from '../role/role.decorator';
 
 @Controller('user')
 @ApiTags('用户模块')
+@UseGuards(AuthGuard)
 export class UserController {
   constructor(private userService: UserService) {}
 
@@ -14,5 +17,15 @@ export class UserController {
   })
   async registUser(@Body() userDto: User) {
     return await this.userService.regist(userDto);
+  }
+
+  @Get('hello')
+  // @SetMetadata('roles', ['admin'])
+  @Role('admin')
+  @ApiOperation({
+    summary: '测试hello',
+  })
+  async hello() {
+    return 'hello world';
   }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,17 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { HashPasswordMiddleware } from 'src/middlewares/hash-password.middleware';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Module({
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [UserController],
 })
 export class UserModule implements NestModule {
